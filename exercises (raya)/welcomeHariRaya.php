533b20655d777c4be3a1292c939499060a12be4a
nurin <165421810+nuriinn@users.noreply.github.com>
2024-05-08 23:34:39 +0800

    Add files via upload

---
+++ exercises (raya)/welcomeHariRaya.php
@@ -0,0 +1,9 @@
+<?php 
+
+	$firstName = "nurin syazwani";
+	$lastName = "zaidi";
+	$welcome_notes = "Selamat Hari Raya " ;
+	
+	echo "Hi my name is $firstName binti $lastName";
+	echo "wish you $welcome_notes";
+?>
